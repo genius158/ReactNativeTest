31cd413fa0b5c34bc9dda991067facb04999c6b6
yan <yxw_developer@outlook.com>
2016-09-29 16:01:56 +0800

    reactNativeTest

--- src/part2.js
+++ src/part2.js
@@ -8,6 +8,7 @@ import {
   AsyncStorage,
   Platform,
   BackAndroid,
+  DatePickerAndroid,
   TouchableHighlight,
 } from 'react-native';
 
@@ -29,17 +30,28 @@ export default class Part2 extends Component{
     }
   }
 
-
   load(){
     AsyncStorage.getItem('one',this.data);
     this.data(null,'sdfsd山东省地方fdfdf');
   }
 
+
   componentWillMount(){
     if (Platform.OS=== 'android') {
       BackAndroid.addEventListener('hardwareBackPress',this.onAndrondBack);
     }
+
+    DatePickerAndroid.open({date:new Date()}).then(
+      result=>{
+        if (result.action=== DatePickerAndroid.dismissAction) {
+          alert('noDate choice');
+        }else {
+          alert(result.year+'年'+(result.month+1)+'月'+result.day+'日');
+        }
+      }
+    )
   }
+
   onAndrondBack=()=>{
     const{navigator}=this.props;
     const routers=navigator.getCurrentRoutes();
@@ -61,7 +73,6 @@ export default class Part2 extends Component{
   }
 
   render(){
-
     this.save();
 
     return(
@@ -77,5 +88,4 @@ export default class Part2 extends Component{
 
     );
   }
-
 }
